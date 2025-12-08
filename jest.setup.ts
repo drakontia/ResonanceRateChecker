ba5be678c9f7e2583c7d0cdb32f6c8684c307d86
Drakontia <mhden@drakontia.com>
2025-12-08 21:07:36 +0900

    Add global config for jest

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,6 @@
 import '@testing-library/jest-dom'
+
+// Mock Next.js globals
+global.Request = class Request {} as any;
+global.Response = class Response {} as any;
+global.Headers = class Headers {} as any;
